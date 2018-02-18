6bbafb0448cf02da02b2a604154b64cdc5b0c47e
ced <zuger.cedric@gmail.com>
2018-02-18 12:07:55 +0100

    Movement hash include rivers and roads.

--- work/build_movement_hash.rb
+++ work/build_movement_hash.rb
@@ -1,18 +1,33 @@
 require 'rhex'
+require 'pp'
 
-map = SquareGridFlatTopped.new
+map = AxialGrid.new
 graph = {}
+MAX_MOVEMENT = 99
 
 movement_table = {
-  c: 0.5, p: 1, f: 2, h: 2, b: 1, r: 0.5
+  c: 0.5, p: 1, f: 2, h: 2, b: 1, w: MAX_MOVEMENT
 }
 
 # Load it with
 map.read_ascii_file_flat_topped_odd( '../data/map.txt' )
 
+rivers = Hash[ JSON.parse( File.open( '../data/rivers.json', 'r' ).read ).map{ |e| [ [ e['q'], e['r'] ], e['c'] ] } ]
+roads = Hash[ JSON.parse( File.open( '../data/roads.json', 'r' ).read ).map{ |e| [ [ e['q'], e['r'] ], e['c'] ] } ]
+
+# pp roads
+
 map.each do |hex|
   map.h_surrounding_hexes( hex ).each do |s_hex|
-    graph[ [ hex.q, hex.r, s_hex.q, s_hex.r ].join( '_' ) ] = movement_table[ s_hex.color ]
+    movement_cost = nil
+    if roads.has_key?( [ hex.q, hex.r ] ) && roads.has_key?( [ s_hex.q, s_hex.r ] )
+      movement_cost = 0.5
+    elsif rivers.has_key?( [ hex.q, hex.r ] ) && roads.has_key?( [ s_hex.q, s_hex.r ] )
+      movement_cost = MAX_MOVEMENT
+    else
+      movement_cost = movement_table[ s_hex.color.downcase ]
+    end
+    graph[ [ hex.q, hex.r, s_hex.q, s_hex.r ].join( '_' ) ] = movement_cost
   end
 end
 
