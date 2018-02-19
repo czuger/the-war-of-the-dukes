40f7b86f80697565e6a8970889cabcc8cc48c3c3
ced <zuger.cedric@gmail.com>
2018-02-19 10:16:00 +0100

    Movement hash reworked.

--- work/build_movement_hash.rb
+++ work/build_movement_hash.rb
@@ -5,7 +5,7 @@ map = AxialGrid.new
 graph = {}
 MAX_MOVEMENT = 99
 
-movement_table = {
+$movement_table = {
   c: 0.5, p: 1, f: 2, h: 2, b: 1, w: MAX_MOVEMENT
 }
 
@@ -13,20 +13,35 @@ movement_table = {
 map.read_ascii_file_flat_topped_odd( '../data/map.txt' )
 
 rivers = Hash[ JSON.parse( File.open( '../data/rivers.json', 'r' ).read ).map{ |e| [ [ e['q'], e['r'] ], e['c'] ] } ]
-roads = Hash[ JSON.parse( File.open( '../data/roads.json', 'r' ).read ).map{ |e| [ [ e['q'], e['r'] ], e['c'] ] } ]
+$roads = Hash[ JSON.parse( File.open( '../data/roads.json', 'r' ).read ).map{ |e| [ [ e['q'], e['r'] ], e['c'] ] } ]
 
 # pp roads
+def over_river?( hex, s_hex )
+  if hex.q == 30 && hex.r == 1 && s_hex.q == 30 && s_hex.r == 2
+    b = true
+  end
+  ( $roads.has_key?( [ hex.q, hex.r ] ) || hex.color.downcase == :c || hex.color.downcase == :b ) &&
+  ( $roads.has_key?( [ s_hex.q, s_hex.r ] ) || s_hex.color.downcase == :c || s_hex.color.downcase == :b )
+end
+
+def bastion?( s_hex )
+  s_hex.color.downcase == 'b'
+end
+
+# puts map.cget( 30, 1 ).color
+# puts $roads.has_key?( [ 30, 2 ] )
 
 map.each do |hex|
   map.h_surrounding_hexes( hex ).each do |s_hex|
     movement_cost = nil
-    if roads.has_key?( [ hex.q, hex.r ] ) && roads.has_key?( [ s_hex.q, s_hex.r ] )
-      movement_cost = 0.5
-    elsif rivers.has_key?( [ hex.q, hex.r ] ) && roads.has_key?( [ s_hex.q, s_hex.r ] )
+    if over_river?( hex, s_hex )
+      movement_cost = bastion?( s_hex ) ? 1 : 0.5
+    elsif rivers.has_key?( [ hex.q, hex.r ] ) && rivers.has_key?( [ s_hex.q, s_hex.r ] )
       movement_cost = MAX_MOVEMENT
     else
-      movement_cost = movement_table[ s_hex.color.downcase ]
+      movement_cost = $movement_table[ s_hex.color.downcase ]
     end
+    # puts "#{[ hex.q, hex.r, s_hex.q, s_hex.r ].join( '_' )} = #{over_river?( hex, s_hex )}"
     graph[ [ hex.q, hex.r, s_hex.q, s_hex.r ].join( '_' ) ] = movement_cost
   end
 end
